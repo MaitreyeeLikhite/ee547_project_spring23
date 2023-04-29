9c4990c9b39ec50d0a63f51259633878826ae09d
Sanskar Tewatia <sanskartewatia@users.noreply.github.com>
2023-04-29 00:06:55 -0700

    log in

--- graphql_test/app.js
+++ graphql_test/app.js
@@ -37,6 +37,10 @@ const schema = buildSchema(`
     addUser(name: String, email: String, phone: String, password: String): User
   }
 `);
+const session = require('express-session');
+
+// ...
+
 
 // Define the root resolver
 const root = {
@@ -55,6 +59,24 @@ app.set('view engine', 'ejs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Configure session middleware
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false
+  }))
+const checkAdminAuthentication = (req, res, next) => {
+    // Check if admin is authenticated (using session-based authentication)
+    const isLoggedIn = req.session.adminLoggedIn;
+  
+    if (isLoggedIn) {
+      // Admin is authenticated, proceed to the next middleware or route handler
+      next();
+    } else {
+      // Admin is not authenticated, redirect to the welcome page
+      res.redirect('/');
+    }
+  };
 // Serve the welcome page
 app.get('/', (req, res) => {
   res.render('welcome');
@@ -66,22 +88,43 @@ app.get('/admin', (req, res) => {
 });
 
 // Handle admin login form submission
-app.post('/admin', (req, res) => {
-  const password = req.body.password;
-  if (password === 'p') {
-    res.redirect('/add-users');
-  } else {
-    res.redirect('/admin');
-  }
-});
+// app.post('/admin', (req, res) => {
+//   const password = req.body.password;
+//   if (password === 'p') {
+//     res.redirect('/add-users');
+//   } else {
+//     res.redirect('/admin');
+//   }
+// });
 
-// Serve the add users page
-app.get('/add-users', (req, res) => {
-  res.render('addUsers');
-});
-
+// // Serve the add users page
+// app.get('/add-users', (req, res) => {
+//   res.render('addUsers');
+// });
+app.get('/add-users', checkAdminAuthentication, (req, res) => {
+    res.render('addUsers');
+  });
 // Handle add users form submission
-
+app.post('/admin', (req, res) => {
+    const { password } = req.body;
+  
+    if (password === 'password') {
+      // Set adminLoggedIn property in the session
+      req.session.adminLoggedIn = true;
+  
+      res.redirect('/add-users');
+    } else {
+      res.redirect('/');
+    }
+  });
+  
+  // Handle admin logout
+  app.get('/admin-logout', (req, res) => {
+    // Unset adminLoggedIn property in the session
+    req.session.adminLoggedIn = false;
+  
+    res.redirect('/');
+  });
 app.post('/add-users', (req, res) => {
     const { name, email, phone, password } = req.body;
     const user = new User({ name, email, phone, password });
@@ -100,6 +143,7 @@ app.post('/add-users', (req, res) => {
 
 // Serve the welcome page (after logging out)
 app.get('/logout', (req, res) => {
+    req.session.adminLoggedIn = false;
     res.render('logout');
   });
   
@@ -107,13 +151,24 @@ app.get('/logout', (req, res) => {
   app.post('/logout', (req, res) => {
     // Perform any necessary logout actions
     // For example, clearing session data or user authentication
-  
+    req.session.adminLoggedIn = false;
     res.redirect('/');
   });
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 // Handle GET request to view user details
-app.get('/details', (req, res) => {
+// app.get('/details', (req, res) => {
+//     // Retrieve user details from the database
+//     User.find()
+//       .then((users) => {
+//         res.render('details', { users });
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         res.redirect('/add-users');
+//       });
+//   });
+app.get('/details', checkAdminAuthentication, (req, res) => {
     // Retrieve user details from the database
     User.find()
       .then((users) => {
